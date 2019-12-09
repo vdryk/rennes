75ba1ddc70c3cb896e107b7ad2189692af45840b
Vincent Dryk <vincent.dryk@gmail.com>
2019-12-09 23:11:39 +0100

    Init project

---
+++ src/main/java/com/is/rennes/RennesApplication.java
@@ -0,0 +1,13 @@
+package com.is.rennes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RennesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RennesApplication.class, args);
+	}
+
+}
