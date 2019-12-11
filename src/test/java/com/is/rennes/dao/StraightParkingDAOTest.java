87d518ee867c41e8a54b080167770dd0b4c9147a
Vincent Dryk <vincent.dryk@gmail.com>
2019-12-11 23:01:04 +0100

    Add one TU with mockito

---
+++ src/test/java/com/is/rennes/dao/StraightParkingDAOTest.java
@@ -0,0 +1,50 @@
+package com.is.rennes.dao;
+
+import com.is.rennes.controller.rest.Parking;
+import com.is.rennes.mapper.ParkingMapper;
+import com.is.rennes.restclient.ParkingRennes;
+import com.is.rennes.restclient.ParkingRestClient;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class StraightParkingDAOTest {
+
+    private StraightParkingDAO straightParkingDAO;
+
+    @Mock
+    private ParkingRestClient parkingRestClient;
+
+    @Mock
+    private ParkingMapper parkingMapper;
+
+    @Before
+    public void setUp() {
+        straightParkingDAO = new StraightParkingDAO(parkingRestClient, parkingMapper);
+    }
+
+    @Test
+    public void shouldGetParkings() {
+        // given
+        ParkingRennes parkingRennes = new ParkingRennes();
+        when(parkingRestClient.fetchRennes()).thenReturn(parkingRennes);
+        List<Parking> expectedParkings = Collections.singletonList(Parking.builder().build());
+        when(parkingMapper.toParking(parkingRennes)).thenReturn(expectedParkings);
+
+        // when
+        List<Parking> actualParkings = straightParkingDAO.getParking("Rennes");
+
+        // then
+        assertEquals(expectedParkings, actualParkings);
+    }
+
+}
